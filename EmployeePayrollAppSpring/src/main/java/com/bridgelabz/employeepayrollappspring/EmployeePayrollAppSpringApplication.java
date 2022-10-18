13726589d1dab4125c09b40e2dc0c020bb359ea9
Hiteshmeher <meher.hitesh4@gmail.com>
2022-10-18 11:02:15 +0530

    [Hitesh]: Using Lombok Library for Logging

--- EmployeePayrollAppSpring/src/main/java/com/bridgelabz/employeepayrollappspring/EmployeePayrollAppSpringApplication.java
+++ EmployeePayrollAppSpring/src/main/java/com/bridgelabz/employeepayrollappspring/EmployeePayrollAppSpringApplication.java
@@ -1,9 +1,11 @@
 package com.bridgelabz.employeepayrollappspring;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class EmployeePayrollAppSpringApplication {
 
     public static void main(String[] args) {
